02819153a55ca44a8297ad73b76e62501822a3f6
thebigjonatron <vanderpluijmgregory@gmail.com>
2023-11-08 10:07:18 +0000

    update

--- secret_consumer/src/main/java/org/example/Main.java
+++ secret_consumer/src/main/java/org/example/Main.java
@@ -1,56 +1,50 @@
 package org.example;
 
-import org.springframework.vault.authentication.KubernetesAuthentication;
-import org.springframework.vault.authentication.KubernetesAuthenticationOptions;
-import org.springframework.vault.authentication.TokenAuthentication;
-import org.springframework.vault.client.VaultEndpoint;
-import org.springframework.vault.core.VaultTemplate;
-
-
-import java.io.*;
-import java.util.Map;
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import com.bettercloud.vault.Vault;
+import com.bettercloud.vault.VaultConfig;
+import com.bettercloud.vault.response.LogicalResponse;
 
 public class Main {
 
-    public static boolean kube_env = true;
-    public static final int port_vault = 8200;
-    public static final String scheme = "http";
 
-    public static VaultEndpoint ve = new VaultEndpoint();
+    /*Default storage path for kubernetes service account*/
+    final static String kube_path = "/var/run/secrets/kubernetes.io/serviceaccount";
+    /*Default storage path for docker compose secrets*/
+    final static String secrets_path = "/run/secrets/vault_token";
+    static final String hostname_sql  = "jdbc:postgresql://"+System.getenv("POSTGRES_ADDR")+"/root";
+    static final String hostname_vault  = "http://" + System.getenv("VAULT_ADDR") + ":8200";
 
-    public static void main(String[] args)  {
+    public static void main(String[] args){
         try {
-            Thread.sleep(3000); // Wait for 5 seconds
-        } catch (InterruptedException ignored) {}
-        final String hostname_vault = System.getenv("VAULT_ADDR");
-        final String host_sql = System.getenv("POSTGRES_ADDR");
-        final String hostname_sql  = "jdbc:postgresql://"+host_sql+"/root";
-        ve.setHost(hostname_vault);
-        ve.setPort(port_vault);
-        ve.setScheme(scheme);
-        /*Setup variables*/
-        String auth_token = find_token("/var/run/secrets/kubernetes.io/serviceaccount");
-        if (auth_token == null) {
-            System.out.println("Kube token not found");
-            auth_token = find_token("/run/secrets/vault_token");
-            System.out.println("Docker-compose token found");
-
-            kube_env = false;
+            Thread.sleep(3000); // Wait for 3 seconds for docker environment to start
+        } catch (InterruptedException ignored) {
+            System.out.println("Can't wait to setup environment");
+        }
+        String username = "";
+        String password = "";
+        try {
+            final VaultConfig config = new VaultConfig()
+                    .address(hostname_vault).engineVersion(1)
+                    .token(getToken()).build();
+            final Vault vault = new Vault(config);
+            LogicalResponse response = vault.logical().read("database/creds/readonly");
+            System.out.println(response.getData());
+            username = response.getData().get("username");
+            password = response.getData().get("password");
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Can't build vault connector at " + hostname_vault);
         }
 
-        /*Connect to vault and read credentials for database*/
-        VaultTemplate vt = vault_connection(auth_token);
-        Map<String, Object> credentials = vt.read("database/creds/readonly").getData();
-
         /*Retrieve username and password from vault response*/
-        final String username = (String) credentials.get("username");
-        final String password = (String) credentials.get("password");
         System.out.println("Database username : " + username + " and password : " + password);
         /*Attempt to connect database*/
         try {
@@ -72,41 +66,35 @@ public class Main {
             e.printStackTrace();
         }
     }
-    /*Returns JWT stored at path*/
-    public static String find_token(String path){
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+    /**
+     * Gets the vault authentication token.
+     * @return vault authentication token.
+     */
+    public static String getToken() {
+        boolean kube_env = false;
+        try (BufferedReader br = new BufferedReader(new FileReader(getPath(kube_env)))) {
             StringBuilder kube_token_bld = new StringBuilder();
             String line;
-            while ((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 kube_token_bld.append(line);
             }
             return kube_token_bld.toString();
 
         } catch (FileNotFoundException e) {
-            /*No token secret found*/
             return null;
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
 
-    public static VaultTemplate vault_connection(String auth_token){
-        if (kube_env){
-            KubernetesAuthenticationOptions options = KubernetesAuthenticationOptions.builder()
-                    .path("kubernetes") // The Kubernetes authentication path in Vault (default is "kubernetes")
-                    .role("my-app-role") // The Kubernetes authentication role in Vault
-                    .jwtSupplier(() -> "YOUR_KUBE_JWT_TOKEN") // A supplier that provides the Kubernetes JWT token
-                    .build();
-            return new VaultTemplate(ve, new KubernetesAuthentication(options, null));
-        } else {
-            return new VaultTemplate(ve, new TokenAuthentication(auth_token));
-        }
+    /**
+     * Returns the vault token path.
+     * @param kube_env True if kubernetes service account.
+     * @return Path to the token.
+     */
+    private static String getPath(boolean kube_env) {
+        return kube_env? kube_path : secrets_path;
     }
-
-
-    /*TODO*/
-    /*Create docker image*/
-    /*Create 3 docker services easy to run mysql, vault and consumer*/
-
 }
